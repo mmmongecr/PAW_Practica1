98ff11e249e49a7c372c949f0db1e95c16a41947
AnndyMadrigal <149017080+AnndyMadrigal@users.noreply.github.com>
2026-06-03 13:47:05 -0600

    Commit Inicial

---
+++ PAW_Practica1/PAW_Practica1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using PAW_Practica1.Models;
+using System.Diagnostics;
+
+namespace PAW_Practica1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PAW_Practica1/PAW_Practica1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PAW_Practica1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
