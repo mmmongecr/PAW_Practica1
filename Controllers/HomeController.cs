b4dc6414b2d58d888520f72c1678ec57d714a1b8
JeremyCoto <jeremycoto2003@gmail.com>
2026-06-03 19:48:31 -0600

    feat: agregar vista de pantalla de inicio (Home) - closes #1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,6 +17,11 @@ namespace PAW_Practica1.Controllers
             return View();
         }
 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
