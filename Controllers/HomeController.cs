d5273f396bcc68e2d200b121b4a70928cdbb39ad
Manuel MM <mmmongecr@hotmail.com>
2026-06-04 17:50:48 -0600

    RecoverPassword+Translate+IndexEnhancement

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,30 @@ namespace PAW_Practica1.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult RecoverPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult RecoverPassword(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                ModelState.AddModelError(string.Empty, "Ingresa un correo electrónico válido.");
+                return View();
+            }
+
+            TempData["SuccessMessage"] = "Si el correo existe, recibirás instrucciones para restablecer tu contraseña.";
+
+            return RedirectToAction(nameof(RecoverPassword));
+        }
+
+        
+    
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
