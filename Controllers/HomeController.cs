3fedd390f9aa42f86744e6b58e7a380c87e63adb
JeremyCoto <jeremycoto2003@gmail.com>
2026-06-03 22:15:08 -0600

    fix: agregar POST en Login para redirigir al Home y actualizar net10.0

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,6 +12,12 @@ namespace PAW_Practica1.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            return RedirectToAction("Index");
+        }
+
         public IActionResult Register()
         {
             return View();
